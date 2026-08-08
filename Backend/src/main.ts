64de525324b0c0b55574e43f3623a44d0d23d531
shokr12 <mshokr1dhdj@gmail.com>
2026-08-08 03:46:34 +0300

    Fix CORS configuration

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,12 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin:"https://resume-boost-iota.vercel.app/",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
+    origin: 'https://resume-boost-iota.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
