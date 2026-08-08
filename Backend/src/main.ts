d710ba3c911d76877227e8d6644a0d6ef51208a5
shokr12 <mshokr1dhdj@gmail.com>
2026-08-08 03:41:02 +0300

    .

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin:"https://resume-boost-iota.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization", "ngrok-skip-browser-warning"],
+    credentials: true,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
